3951de5f022851ff9dac61623c1f3bb54747e3c0
kopadze <kopadzejemo@gmail.com>
2019-05-27 21:54:46 +0400

    feat(02-control-flow-loops): Finish Task-1

---
+++ 02-control-flow-loops/task-1.js
@@ -0,0 +1,5 @@
+var result, a=2, b=3;
+
+result=a+b < 4 ? true : false;
+
+console.log(result);
---
+++ 02-control-flow-loops/task-2.js
@@ -0,0 +1,10 @@
+var message, login = 'Owner';
+
+message = login =='Pitter' ? message = 'Hi':
+login = 'Owner' ? message = 'Hello':
+login = '' ? message = 'unknow': message = '';
+
+console.log(message)
+
+
+

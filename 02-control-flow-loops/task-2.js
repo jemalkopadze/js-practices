c40031778543bc47956337f49c3ffefc3bb25f49
jemalkopadze <kopadzejemo@gmail.com>
2019-05-31 21:19:17 +0400

    fix(02-control-flow-loops): fixed task-2

--- 02-control-flow-loops/task-2.js
+++ 02-control-flow-loops/task-2.js
@@ -1,10 +1,10 @@
-let message, login = 'Owner';
+let message, login = '';
 
 message = login =='Pitter' ? message = 'Hi':
-login = 'Owner' ? message = 'Hello':
-login = '' ? message = 'unknow': message = '';
+login == 'Owner' ? message = 'Hello':
+login == '' ? message = 'unknow': message = '';
 
-console.log(message)
+console.log(message);
 
 
 
